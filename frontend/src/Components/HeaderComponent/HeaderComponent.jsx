4381957c0fcbda93931229c46c6ece6ccb3cc36b
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-06-20 13:30:22 +0530

    workflow id check x2

--- frontend/src/Components/HeaderComponent/HeaderComponent.jsx
+++ frontend/src/Components/HeaderComponent/HeaderComponent.jsx
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
   return (
     <>
        <nav className='navbar container pt-3 pb-3 rounded align-items-start'>
-            <Link to="/" className='navbar-brand text-light'>Portal Version check x6</Link>
+            <Link to="/" className='navbar-brand text-light'>Portal Version check x7</Link>
 
             <div>
                 <ButtonComponent url='/login' text='login' class='btn btn-outline-info' />
